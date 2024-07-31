0bda7f92418cb2a209867f5c80836c2426013491
arrowban <thearrowban@gmail.com>
2024-07-31 03:24:36 -0400

    Refactor web app types, update README

--- apps/web/src/types/app.js
+++ apps/web/src/types/app.js
@@ -1,23 +1,4 @@
 /**
- * @typedef {object} User
- * @property {string} id
- * @property {string} created
- * @property {string} updated
- * @property {string} username
- * @property {string=} email
- * @property {boolean} emailVisibility
- * @property {boolean} verified
- * @property {string} name
- * @property {string} avatar
- *
  * @typedef {import("svelte/store").Writable<string>} TokenStore
  * @typedef {import("svelte/store").Writable<User | null>} UserStore
- *
- * @typedef {object} Post
- * @property {string} id
- * @property {string} created
- * @property {string} updated
- * @property {string} user
- * @property {number} count
- * @property {{user: User}=} expand
  */
--- apps/web/src/types/db.js
+++ apps/web/src/types/db.js
@@ -1,8 +1,27 @@
 /**
  * @typedef {import("pocketbase").RecordModel } RecordModel
  *
+ * @typedef {object} User
+ * @property {string} id
+ * @property {string} created
+ * @property {string} updated
+ * @property {string} username
+ * @property {string=} email
+ * @property {boolean} emailVisibility
+ * @property {boolean} verified
+ * @property {string} name
+ * @property {string} avatar
+ *
  * @typedef {RecordModel & User } UserRecordModel
  * @typedef {import("pocketbase").RecordAuthResponse<UserRecordModel> } UserAuthRefresh
+ *
+ * @typedef {object} Post
+ * @property {string} id
+ * @property {string} created
+ * @property {string} updated
+ * @property {string} user
+ * @property {number} count
+ * @property {{user: User}=} expand
  *
  * @typedef {RecordModel & Post} PostRecordModel
  */
